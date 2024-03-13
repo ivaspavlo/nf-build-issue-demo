fc7da5b82344e661ae071859e1c1de294f088008
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-03-13 14:35:00 +0100

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fi-mfe',
+    loadChildren: () => loadRemoteModule('fi-mfe', './routes').then((m: any) => m.FI_MFE_ROUTES)
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// @ts-ignore
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+initFederation('/assets/federation.manifest.json')
+  .catch((err: any) => console.error(err))
+  .then(() => import('./bootstrap'))
+  .catch((err: any) => console.error(err));
